886cd49b7b0472f25b60addaf60e35cf307ff68b
ankit <itsmeankit804@gmail.com>
2026-05-27 10:46:28 +0545

    javasc

---
+++ gradesystem.js
@@ -0,0 +1,13 @@
+const marks= 70;
+if(marks >=90){
+    console.log("YOU HAVE SCORED A+")
+}else if(marks >=80 && marks<90){
+    console.log("YOU HAVE SCORED  A")
+
+}else if(marks >=70 && marks< 80){
+    console.log("YOU HAVE SCORED B+")
+}else if(marks > 60){
+    console.log("YOU HAVE SCORED C+")
+}else{
+    console.log("Failed!!!")
+}
