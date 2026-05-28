7a1be58084d4f972c5152ea12e475b8b039a41fa
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2026-05-28 21:11:21 +0545

    day 4

---
+++ string.js
@@ -0,0 +1,12 @@
+let name = "Nishan Kumar Rai";
+// index       character 
+// 0               N
+// 1               i
+
+console.log(name[0])
+console.log(name.length)
+
+
+// String Methods 
+// 1. toUpperCase()
+// console.log(name.toUppercase())
